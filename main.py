e3c2db3de35aff0a6c3b4915d3626e7917ec3f8f
wangding <wangding@ezbuy.com>
2018-10-19 11:27:05 +0800

    Initialize

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+-----------------------------------------
+    Project Name:   AICrawler
+    File Name:      main
+    Author:         wangding
+    Create Date:    2018/10/18 下午5:37
+-----------------------------------------
+"""
+
